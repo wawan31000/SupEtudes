a7a2f343b1179022bf29f3b2c165bd79f6ed0936
Developer <developer@Mac-de-Developer.local>
2014-01-21 12:43:39 +0100

    ajout model Informations

---
+++ SupEtudes/MROInformations.m
@@ -0,0 +1,20 @@
+//
+//  MROInformations.m
+//  SupEtudes
+//
+//  Created by Developer on 21/01/14.
+//  Copyright (c) 2014 Maxime Roch. All rights reserved.
+//
+
+#import "MROInformations.h"
+
+@implementation MROInformations
+- (id)initWithNote:(int) newNote
+{
+    self = [super init];
+    if (self) {
+        _note=newNote;
+    }
+    return self;
+}
+@end

deb3f8517e9dc14e4a59c1b785640c6cff33751e
Developer <developer@Mac-de-Developer.local>
2014-01-21 10:15:28 +0100

    ajout coredata manager

---
+++ SupEtudes/MROCoreDataManager.h
@@ -0,0 +1,21 @@
+//
+//  MROCoreDataManager.h
+//  SupEtudes
+//
+//  Created by Developer on 21/01/14.
+//  Copyright (c) 2014 Maxime Roch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface MROCoreDataManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
